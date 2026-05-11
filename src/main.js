56d715f8401c7188065659b8361bd2c795ed4423
ludokyeho <84244228+ludokyeho@users.noreply.github.com>
2026-05-11 16:16:01 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -21,7 +21,7 @@ const revenueCategories = {
   Divers: ["Cours particuliers", "Remboursements", "Autres"],
 };
 
-const colors = ["#111827", "#0f766e", "#d97706", "#2563eb", "#be123c", "#7c3aed", "#4b5563", "#059669", "#c2410c","#0284c7"];
+const colors = ["#111827", "#0f766e", "#d97706", "#2563eb", "#be123c", "#7c3aed", "#0284c7", "#059669", "#c2410c"];
 
 let state = loadLocalState();
 let ui = {
